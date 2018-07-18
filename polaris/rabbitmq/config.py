302bbdc459c098b0bf42669151ec63bfcc185e54
sh8 <s@sh8.io>
2018-07-18 19:05:08 +0900

    Fix typo

--- polaris/rabbitmq/config.py
+++ polaris/rabbitmq/config.py
@@ -5,7 +5,7 @@ RABBITMQ_URL = os.getenv('RABBITMQ_URL', 'amqp://localhost//')
 url = urlparse(RABBITMQ_URL)
 
 RABBITMQ_HOST = url.hostname
-RABBITMQ_USERNAME = url.usename
+RABBITMQ_USERNAME = url.username
 RABBITMQ_PASSWORD = url.password
 RABBITMQ_PORT = 5672
 RABBITMQ_VIRTUAL_HOST = url.path[1:]
