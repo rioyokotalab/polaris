ff3e8668c6967c67013d75d96dd33b01273e0319
sh8 <s@sh8.io>
2018-07-19 02:22:45 +0900

    Fix project name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_long_description():
 
 
 setup(
-    name='polaris',
+    name='polaris-py',
     packages=find_packages(),
     scripts=['bin/polaris-worker'],
     version='0.1',
@@ -27,7 +27,7 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX',
         'Operating System :: Unix',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6'
         ],
     platforms=['Linux', 'OS-X'],
